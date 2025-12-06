7d7f86c3093080c4759ff76927ab030902b6a668
GannouniAmine <Mohamedamine.gannouni@ensi-uma.tn>
2025-12-06 14:25:59 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MedAmineGannouni-portfelio/',
+  base: '/portfolio_MohamedamineGannouni/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
